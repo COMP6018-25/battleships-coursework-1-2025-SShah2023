62c1d7670be1e98130f923a8dc065038ff47e96e
SShah2023 <19030309@brookes.ac.uk>
2025-05-01 22:03:57 +0100

    Reduced number of test. annotated three tests describing the scenario as per the requirements

--- test/model/GameModelTest.java
+++ test/model/GameModelTest.java
@@ -34,30 +34,11 @@ class GameModelTest {
     }
 
     @Test
-    void testPlaceShipOverlappingFails() {
-        Ship ship1 = new Ship(3, "1");
-        Ship ship2 = new Ship(3, "2");
-        assertTrue(game.placeShip(ship1, 0, 0, true));
-        assertFalse(game.placeShip(ship2, 0, 1, true), "Overlapping ships should not be placed.");
-    }
-
-    @Test
     void testMakeGuessHit() {
         Ship ship = new Ship(2, "2");
         game.placeShip(ship, 1, 1, true);
         assertTrue(game.makeGuess(1, 1), "Guess on ship should be a hit.");
     }
-
-    @Test
-    void testMakeGuessMiss() {
-        assertFalse(game.makeGuess(5, 5), "Guess on empty cell should be a miss.");
-    }
-
-    @Test
-    void testGameOverFalseAtStart() {
-        assertFalse(game.isGameOver(), "Game should not be over at the start.");
-    }
-
 
      //Scenario: The game should end when all ships are sunk.
      //A single ship is placed and hit until sunk to trigger game over.
